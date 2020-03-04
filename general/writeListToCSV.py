bb0cad9f78ad8c2a7c3b2fd9daa67fda2be0c489
Clemens Jaenicke <clemens.jaenicke@geo.hu-berlin.de>
2020-03-04 15:12:05 +0100

    Optimized function, removed if clause

--- general/writeListToCSV.py
+++ general/writeListToCSV.py
@@ -1,19 +1,33 @@
-def writeListToCSV(inputList, outputFile):
+def writeListToCSV(in_lst, out_pth, sep = ","):
     """
-    Write a provided list with several sublists(i.e. the rows) into a csv File. Every sublist is written into one line.
-    :param inputList: List that contains several sublists. List[Sublist,Sublist,etc]
-    :param outputFile: Name of output File with ending .csv. String
+    Writes a provided list with several sub lists (which will represent the rows) into a csv File.
+    Every sub list is written into one line.
+    :param in_lst: List that contains several sub lists: List[Sublist,Sublist,etc]
+    :param out_pth: Path, including name of output file with ending .csv. String
+    :param sep: Optional, specify a separator, default: comma. String
     :return: Returns provided list as csv file.
     """
-    outputFile = open(outputFile, 'w')
-    for subList in inputList:
-        i = 0
-        for subitem in subList:
-            if i < len(inputList[0]) - 1:
-                outputFile.write(str(subitem))
-                outputFile.write(",")
-                i += 1
-            else:
-                outputFile.write(str(subitem))
-        outputFile.write('\n')
-    del outputFile
+    out_file = open(out_pth, 'w')
+    for sub_lst in in_lst:
+        length = len(sub_lst)
+
+        for i in range(length-1):
+            item = sub_lst[i]
+            out_file.write(str(item))
+            out_file.write(sep)
+
+        item = sub_lst[length-1]
+        out_file.write(str(item))
+        out_file.write('\n')
+
+    del out_file
+
+    #     for item in sub_lst:
+    #         if i < len(inputList[0]) - 1:
+    #             outputFile.write(str(item))
+    #             outputFile.write(",")
+    #             i += 1
+    #         else:
+    #             outputFile.write(str(item))
+    #     outputFile.write('\n')
+    # del outputFile
