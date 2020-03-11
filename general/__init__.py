3545ba014eaf8d6dc7b2c05e3d11f550b786efc3
Clemens Jaenicke <clemens.jaenicke@geo.hu-berlin.de>
2020-03-11 15:12:35 +0100

    Added new functions

--- general/__init__.py
+++ general/__init__.py
@@ -3,4 +3,5 @@ from .findBetween import findBetween
 from .findDuplicatesInList import findDuplicatesInList
 from .generatePosOrNegRandInt import generatePosOrNegRandInt
 from .getFilesInFolderWithEnding import getFilesinFolderWithEnding
+from .orderAndConcatItems import orderAndConcatItems
 from .writeListToCSV import writeListToCSV
