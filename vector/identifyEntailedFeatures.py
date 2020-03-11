1c4b8fdc602f6014de137252dbd9ad9b9d0c56b3
Clemens Jaenicke <clemens.jaenicke@geo.hu-berlin.de>
2020-03-11 15:14:46 +0100

    Function to filter entailed features

--- vector/identifyEntailedFeatures.py
+++ vector/identifyEntailedFeatures.py
@@ -1,13 +1,12 @@
-def removeEntailedFeatures(in_shp_pth,  id_field="ID", margin = 0):
+def identifyEntailedFeatures(in_shp_pth,  id_field="ID", margin = 0):
     """
 
     :param in_shp_pth:
     :param id_field:
     :return:
     """
-    import os
-    import ogr
     import vector
+    import ogr
 
     in_shp = ogr.Open(in_shp_pth, 0)
     in_lyr = in_shp.GetLayer()
@@ -19,43 +18,52 @@ def removeEntailedFeatures(in_shp_pth,  id_field="ID", margin = 0):
     for feat_curr in in_lyr:
 
         id1 = feat_curr.GetField(id_field)
-        print("FEATURE: {}".format(id1))
+        # print("FEATURE: {}".format(id1))
         geom_curr = feat_curr.GetGeometryRef()
-        area_curr = geom_curr.Area()
-        copy_lyr.SetSpatialFilter(geom_curr)
+        geom_curr = geom_curr.Buffer(margin)
 
+        copy_lyr.SetSpatialFilter(geom_curr)
 
         for feat_nb in copy_lyr:
             id2 = feat_nb.GetField(id_field)
             geom_nb = feat_nb.GetGeometryRef()
-            area_nb = geom_nb.Area()
+
             if id1 != id2:
-                # if geom_nb.Within(geom_curr):
-                # if geom_curr.Within(geom_nb):
-                #     out_lst.append(id2)
-                #     print("Feature {} is within feature {}".format(id2, id1))
-                intersection = geom_nb.Intersection(geom_curr)
-                geom_type = intersection.GetGeometryName()
-                if geom_type not in ['POLYGON',
-                                     'MULTIPOLYGON']:  # in ['MULTILINESTRING', 'POINT', 'LINESTRING','MULTIPOINT']: #alternatively
-                    intersection = None
-                    area_inters = 0
-                    # print("Intersection of {} and {} is a {}".format(id1, id2, geom_type))
-                elif intersection != None:
-                    area_inters = intersection.Area()
-                    # print("Intersection of {} and {} is NOT none. Its area is {} and its geom type is {}".format(id1, id2, area_inters, geom_type))
-                else:
-                    intersection = None
-                    area_inters = 0
+                if geom_nb.Within(geom_curr):
 
-                print("Area 1: {} \n Area 2: {} \n Area I: {}".format(area_curr, area_nb, area_inters))
-
-                if round(area_inters,0)  == round(area_nb,0):
                     out_lst.append(id2)
-                    print("Feature {} is within feature {}".format(id2, id1))
-
 
         copy_lyr.ResetReading()
     in_lyr.ResetReading()
 
     return(out_lst)
+
+    ## SLOW VERSION
+    # import ogr
+    #
+    # in_shp = ogr.Open(in_shp_pth, 0)
+    # in_lyr = in_shp.GetLayer()
+    #
+    # num_feat = in_lyr.GetFeatureCount()
+    #
+    # out_lst = []
+    #
+    # for n in range(num_feat):
+    #     feat_curr = in_lyr.GetFeature(n)
+    #     fid = feat_curr.GetField(id_field)
+    #     # print("FEATURE: {}".format(fid))
+    #
+    #     geom_curr = feat_curr.GetGeometryRef()
+    #     geom_curr = geom_curr.Buffer(margin)
+    #
+    #     in_lyr.SetSpatialFilter(geom_curr)
+    #
+    #     for feat_nb in in_lyr:
+    #         fid_nb = feat_nb.GetField(id_field)
+    #         geom_nb = feat_nb.GetGeometryRef()
+    #         if fid != fid_nb:
+    #             if geom_nb.Within(geom_curr):
+    #                 out_lst.append(fid_nb)
+    #     in_lyr.ResetReading()
+    #
+    # return (out_lst)
